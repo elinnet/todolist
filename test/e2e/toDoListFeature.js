66e39322f874cfc430f6aea62b836c0e7098a14d
Elin <elinnet@gmail.com>
2015-08-01 20:06:26 +0100

    protractor test on list displaying newly input tasks passsing

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,7 +1,17 @@
-describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
+describe('To Do List', function() {
 
+  beforeEach(function(){
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('To Do List');
   });
+
+  it('displays a newly input task', function(){
+    element(by.model('inputBox')).sendKeys('do laundry');
+    element(by.id('submit')).click();
+    expect(element(by.binding('tasks.item')).getText()).toEqual('do laundry');
+  });
+
 });
